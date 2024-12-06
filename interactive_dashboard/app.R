6da3aaa642fdd0f480f72c527316c9b5b5b7707b
NowellPhelps <nowell.phelps@gmail.com>
2024-12-06 13:52:26 -0500

    working interactive scatters

--- interactive_dashboard/app.R
+++ interactive_dashboard/app.R
@@ -8,6 +8,21 @@ library(tidyverse)
 library(ranger)
 
 # Read data
+
+## get Range of bean variables for slider inputs
+data <- beans
+names(data)
+beans_summary_stats <- data.frame(cbind(t(beans %>% select(-"class") %>% summarise_all(min)), 
+                             t(beans %>% select(-"class") %>% summarise_all(max)),
+                             t(beans %>% select(-"class") %>% summarise_all(.funs = function(x) min(x)/2 + max(x)/2))))
+
+names(beans_summary_stats) <- c("min", "max", "mid")
+beans_summary_stats <- cbind(data.frame("var" = rownames(beans_summary_stats)),
+                             beans_summary_stats)
+rownames(beans_summary_stats) <- NULL
+
+vars <- beans_summary_stats$var
+
 set.seed(250)
 bean_split = initial_split(beans, prop = 0.8, strata = class)
 bean_training <- training(bean_split)
@@ -31,6 +46,30 @@ ui <- fluidPage(
   
   navbarPage("",
              
+             tabPanel("Explore dataset", 
+                      
+                      sidebarLayout(
+                        
+                        # Sidebar panel for inputs ----
+                        sidebarPanel(
+                          
+                          
+                          # Input: Selector for model
+                          selectInput(inputId = "data_plot.x",
+                                      label = "Choose a variable to plot on x axis:",
+                                      choices = vars),
+                          
+                          selectInput(inputId = "data_plot.y",
+                                      label = "Choose a variable to plot on y axis:",
+                                      choices = vars)),
+                        
+                        
+                        mainPanel(
+                          
+                          plotOutput("scatter_plot")
+                        )
+                      )),
+             
              tabPanel(
                "Interactively explore model",
                
@@ -39,29 +78,37 @@ ui <- fluidPage(
                  # Sidebar panel for inputs ----
                  sidebarPanel(
                    
-                   # Input: Selector for choosing outcome ----
+                   # Input: Selector for choosing visualisation
                    selectInput(inputId = "outcome",
                                label = "Choose an outcome:",
                                choices = c("Diarrhea under 5s")),
                    
-                 ),
-              
+                    
+                     
+                     # Input: Selector for model
+                     selectInput(inputId = "model_type",
+                                 label = "Choose a model:",
+                                 choices = c("Random Forest",
+                                             "XGBoost")),
                  
-                 # Input: Selector for choosing input ----
-                 selectInput(inputId = "input",
-                             label = "Choose a model:",
-                             choices = c("Random Forest",
-                                         "XGBoost"))),
+                 
+                 # Sliders
+                 "Sliders to choose variable values:",
+                 
+                 sliderInput(inputId = "area", 
+                             label = "area:",
+                             min = beans_summary_stats$min[which(beans_summary_stats$var == "area")], 
+                             max = beans_summary_stats$max[which(beans_summary_stats$var == "area")],
+                             value = beans_summary_stats$mid[which(beans_summary_stats$var == "area")])
+                 
+                 ),
+                   
                  
                  mainPanel(
-                   textOutput("outcome_title"),
-                   verbatimTextOutput("outcome_summary"),
-                   textOutput("input_title"),
-                   verbatimTextOutput("input_summary"),
-                   plotOutput("plot")
+                   "Main panel"
                    
                  )
-               ),
+               )),
              
              tabPanel("Model Training"), 
              
@@ -73,13 +120,28 @@ ui <- fluidPage(
 # Define your Shiny server logic here
 server <- function(input, output, session){
   
-  # Return the requested dataset ----
-  datasetOutcome <- reactive({
-    switch(input$outcome, 
-           "Diarrhea under 5s" = "diarrhea_under_5")
+  output$scatter_plot <- renderPlot({
+    
+    plot_data <- data
+    
+    plot_data$xvar <- unlist(data[,input$data_plot.x])
+    
+    plot_data$yvar <- unlist(data[,input$data_plot.y])
+    
+    colnames(plot_data)[c(ncol(plot_data)-1, ncol(plot_data))] <- c("xvar", "yvar")
+    
+    ggplot(plot_data, aes(x = xvar, y = yvar, colour = class)) +
+      geom_point() +
+      theme_minimal()
+    
+    
   })
   
+  
 }
+
+
+
 
 # Create and launch the Shiny app
 shinyApp(ui, server)
