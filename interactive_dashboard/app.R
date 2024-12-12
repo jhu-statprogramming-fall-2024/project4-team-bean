d9823fbf87ed2a2fc9ccde26dc71e942dfacf15b
NowellPhelps <nowell.phelps@gmail.com>
2024-12-12 08:40:53 -0500

    functional (not pretty) dashboard

--- interactive_dashboard/app.R
+++ interactive_dashboard/app.R
@@ -1,30 +1,20 @@
+############# Initialization and background code ###############################
+
+# Load required packages
 rm(list = ls())
 library(tidymodels)
 library(beans)
 library(bundle)
 library(shiny)
-library(rsconnect)
 library(tidyverse)
 library(ranger)
 library(stringr)
 library(cowplot)
+library(xgboost)
+library(kernlab)
+library(grid)
 
-# Read data
-
-## get Range of bean variables for slider inputs
-data <- beans
-names(data)
-beans_summary_stats <- data.frame(cbind(t(beans %>% select(-"class") %>% summarise_all(min)), 
-                             t(beans %>% select(-"class") %>% summarise_all(max)),
-                             t(beans %>% select(-"class") %>% summarise_all(.funs = function(x) min(x)/2 + max(x)/2))))
-
-names(beans_summary_stats) <- c("min", "max", "mid")
-beans_summary_stats <- cbind(data.frame("var" = rownames(beans_summary_stats)),
-                             beans_summary_stats)
-rownames(beans_summary_stats) <- NULL
-
-vars <- beans_summary_stats$var
-
+# Define utility function for application
 get_var_longname <- function(var){
   switch(var, 
          "area" = "Area (square pixels)",
@@ -44,35 +34,98 @@ get_var_longname <- function(var){
          "shape_factor_3" = "Shape Factor 3",
          "shape_factor_4" = "Shape Factor 4")}
 
+
+# Read in machine learning models and save in list
+model_names <- c("Random Forest" = "RF",
+                 "Lasso Regression" = "Lasso",
+                 "XGBoost" = "XG",
+                 "Lasso on principle components" = "Lasso_PCA",
+                 "Support Vector Machine on principle components" = "SVM_PCA",
+                 "Ensemble prediction using all models" = "RF_XG_Lasso_LassoPCA_SvmPCA")
+
+models <- list()
+
+model <- "Lasso_PCA"
+load(paste0(model, "_predict.RData"))
+
+
+for (model in model_names){
+  
+  load(paste0(model, "_predict.RData"))
+  
+  # switch names as needed (there are inconsistencies between file names and bundle names)
+  model <- ifelse(model == "RF", "rf",
+                  ifelse(model == "Lasso","lasso", 
+                         ifelse(model == "RF_XG_Lasso_LassoPCA_SvmPCA", "ensemble_5", 
+                                ifelse(model == "SVM_PCA", "svm_PCA", 
+                                       ifelse(model == "XG", "xg", 
+                                              ifelse(model == "Lasso_PCA", "lasso_PCA", model))))))
+  
+  model_object <- unbundle(get(paste0(model, "_bundle")))
+  
+  # switch names back as needed (there are inconsistencies between file names and bundle names)
+  model <- ifelse(model == "rf", "RF", 
+                  ifelse(model == "lasso","Lasso", 
+                         ifelse(model == "ensemble_5","RF_XG_Lasso_LassoPCA_SvmPCA", 
+                                ifelse(model == "svm_PCA", "SVM_PCA", 
+                                       ifelse(model == "xg", "XG", 
+                                              ifelse(model == "lasso_PCA", "Lasso_PCA", model))))))
+  
+  # Save unbundled model objesct into list
+  models[[names(model_names)[which(model_names == model)]]] <- model_object
+}
+
+# Get ranges of features for slider inputs in dashboard
+data <- beans
+names(data)
+
+beans_summary_stats <- data.frame(cbind(t(beans %>% select(-"class") %>% summarise_all(min)), 
+                             t(beans %>% select(-"class") %>% summarise_all(max)),
+                             t(beans %>% select(-"class") %>% summarise_all(.funs = function(x) min(x)/2 + max(x)/2))))
+
+names(beans_summary_stats) <- c("min", "max", "mid")
+beans_summary_stats <- cbind(data.frame("var" = rownames(beans_summary_stats)),
+                             beans_summary_stats)
+
+rownames(beans_summary_stats) <- NULL
+
+bean_names <- unique(data$class)
+  
+col_scale <- scale_color_manual(values = c("barbunya" = "#1B9E77",
+                                          "bombay" =  "#D95F02",
+                                          "cali" = "#7570B3",
+                                          "dermason" = "#E7298A",
+                                          "horoz" = "#66A61E",
+                                          "seker" = "#E6AB02",
+                                          "sira" = "#A6761D"))
+
+fill_scale <- scale_fill_manual(values = c("barbunya" = "#1B9E77",
+                                            "bombay" =  "#D95F02",
+                                            "cali" = "#7570B3",
+                                            "dermason" = "#E7298A",
+                                            "horoz" = "#66A61E",
+                                            "seker" = "#E6AB02",
+                                            "sira" = "#A6761D"))
+
+# Save summaries of features 
+vars <- beans_summary_stats$var
 vars_pretty <- sapply(X = vars, FUN = function(X) get_var_longname(X))
 names(vars) <- vars_pretty
 
+# Save training and test dataset 
 set.seed(250)
 bean_split = initial_split(beans, prop = 0.8, strata = class)
 bean_training <- training(bean_split)
 bean_testing <- testing(bean_split)
 
-# Read in random forest
-rf.data.file.name <- "RF_predict.RData"
-load(rf.data.file.name)
 
-set.seed(runif(890))
-sample_test <- sample_n(bean_testing, 2)
+################################### APP ########################################
 
-model_object <- unbundle(rf_bundle)
-table(as.character(predict(model_object, bean_testing)[[1]]), as.character(bean_testing$class))
-
-models <- list()
-models[["Random Forest"]] <- model_object
-
-
-# Define your Shiny UI here
+# Define Shiny UI here
 ui <- fluidPage(
   
   # App title 
   titlePanel("Machine learning models for bean classification - TEAM BEAN"),
-  
-    
   
   navbarPage("",
              
@@ -97,10 +150,6 @@ More details on bean classes, and the creation of the dataset, including how the
                         
                         # Sidebar panel for inputs ----
                         sidebarPanel(
-                          selectInput(inputId = "model_type",
-                                      label = "Choose a model:",
-                                      choices = c("Random Forest",
-                                                  "XGBoost")),
                           
                           # Input: Selector for model
                           selectInput(inputId = "data_plot.x",
@@ -111,7 +160,8 @@ More details on bean classes, and the creation of the dataset, including how the
                                       label = "Choose a variable to plot on y axis:",
                                       selected = "perimeter",
                                       choices = vars),
-                        # Sliders
+                          
+                        # Input: sliders for variable value choices
                        "Choose variable values:",
                        " ",
                        
@@ -231,7 +281,7 @@ More details on bean classes, and the creation of the dataset, including how the
                                  round = -4)),
                         
                         mainPanel(
-                          
+                          h1(strong("Data visualisation"), style = "font-size:30px;"),
                           strong(textOutput("plot_title")),
                           fluidRow(
                             column(6, plotOutput(outputId = "beans_legend", width  = "400px",height = "400px")),
@@ -246,22 +296,20 @@ More details on bean classes, and the creation of the dataset, including how the
                             
                             
                           strong("Model agreement and disagreement..."),
-                          tableOutput("model_prediction")
+                          plotOutput("model_prediction")
                         )
-                      ))),
-             
-             
-             tabPanel("Model Training"), 
-             
-             tabPanel("Interactive Component"), 
-             
-             tabPanel("Classify multiple observations"))
+                      )
+                     ), 
 
 
-# Define your Shiny server logic here
+            tabPanel("Upload dataset")
+
+))
+
+# Define Shiny server logic
 server <- function(input, output, session){
   
-  # Reactive expression to create data frame of all input values ----
+  # Expression to create data frame of all input values
   sliderValues <- reactive({
     
     data.frame(
@@ -301,10 +349,12 @@ server <- function(input, output, session){
     
   })
   
+  # Create plot title
   output$plot_title <- renderText({
     str_to_sentence(paste0(get_var_longname(input$data_plot.y), " against ", get_var_longname(input$data_plot.x), ", by bean class"))
     })
   
+  # Create scatter plot
   output$scatter_plot <- renderPlot({
     
     plot_data <- bean_training
@@ -321,6 +371,7 @@ server <- function(input, output, session){
     
     ggplot(plot_data, aes(x = xvar, y = yvar, colour = class)) +
       geom_point(alpha = 0.4) +
+      col_scale +
       theme_classic() +
       geom_point(inherit.aes = F, colour = "red", x = user_xval, y = user_yval, shape = 4, size = 6) +
       labs(x = get_var_longname(input$data_plot.x), 
@@ -329,6 +380,7 @@ server <- function(input, output, session){
     
   })
   
+  # Create histogram of feature on x axis of scatter plot
   output$histogram_x <- renderPlot({
     
     plot_data <- bean_training
@@ -346,7 +398,8 @@ server <- function(input, output, session){
     ggplot(plot_data, aes(x = xvar, group = class, fill = class)) +
       geom_density(linewidth = 0.5, colour = "white", alpha = 0.5) +
       theme_classic() +
-      geom_vline(inherit.aes = F, xintercept = user_xval, colour = "red") +
+      fill_scale +
+      geom_vline(xintercept = user_xval, colour = "red") +
       labs(x = get_var_longname(input$data_plot.x),
            y = "Count") +
       theme(axis.text.y = element_blank(),
@@ -359,6 +412,7 @@ server <- function(input, output, session){
     
   })
   
+  # Create histogram of feature on y axis of scatter plot
   output$histogram_y <- renderPlot({
     
     plot_data <- bean_training
@@ -376,7 +430,8 @@ server <- function(input, output, session){
     ggplot(plot_data, aes(x = yvar, group = class, fill = class)) +
       geom_density(linewidth = 0.5, colour = "white", alpha = 0.5) +
       theme_classic() +
-      geom_vline(inherit.aes = F, xintercept = user_yval, colour = "red") +
+      fill_scale +
+      geom_vline(xintercept = user_yval, colour = "red") +
       labs(x = get_var_longname(input$data_plot.y),
            y = "Count") + 
       theme(axis.text.y = element_blank(),
@@ -391,6 +446,7 @@ server <- function(input, output, session){
     
   })
   
+  # Create legend for plots
   output$beans_legend <- renderPlot({
     
     plot_data <- bean_training
@@ -408,37 +464,54 @@ server <- function(input, output, session){
     p <- ggplot(plot_data, aes(x = yvar, group = class, fill = class)) +
       geom_density(linewidth = 0.5, colour = "white", alpha = 0.5) +
       theme_classic() +
-      geom_vline(inherit.aes = F, xintercept = user_yval, colour = "red") +
+      fill_scale +
+      geom_vline(xintercept = user_yval, colour = "red") +
       labs(title = paste0("Marginal distribution of ",get_var_longname(input$data_plot.x), ", by bean class"), 
            x = get_var_longname(input$data_plot.y),
            y = "Count") +
       coord_flip()
     
-    legend <- get_legend(p)
+    a <- get_legend(p)
     
-    plot(legend)
+    grid.draw(a)
     
   })
   
-  
-  output$model_prediction <- renderTable({
-
+  # Plot model predictions as heatmap
+  output$model_prediction <- renderPlot({
+    
     inputs <- sliderValues()
-    inputs <- data.frame(inputs)
     
     inputs <- inputs %>% pivot_wider(id_cols = NULL, names_from = Name, values_from = Value)
     
-    model_name <- input$model_type
-    model <- models[[model_name]]
+    predictions <- c()
     
-    prediction <- predict(models[["Random Forest"]], inputs)
+    for (model in names(model_names)){
+      
+      predictions <- c(predictions, as.character(predict(models[[input$model_type]], inputs)[[1]]))
+      
+    }
     
-    paste0("The ", tolower(model_name), " model predicts that a bean with the supplied feature values is a ", as.character(prediction[[1]]), " bean")
+    predictions_df <- data.frame("Model" = names(model_names),
+                                "Classification" = predictions)
+    
+    all_predictions_df <- data.frame("Model" = rep(names(model_names), length(bean_names)),
+                                 "Classification" = rep(bean_names, each = length(model_names)),
+                                 "pred" = rep(NA, length(bean_names) * length(model_names)))
+    
+    predictions_df$Classification <- factor(predictions_df$Classification, bean_names)
+    
+    ggplot(all_predictions_df, aes(x = Model, y = Classification)) +
+      geom_tile(linewidth=0.5, colour ="black", fill = "white") +
+      geom_tile(data = predictions_df, aes(fill = Classification), linewidth=0.5, colour ="black") +
+      fill_scale + 
+      theme_classic() +
+      theme(legend.position = "none") +
+      theme(axis.line = element_blank())
     
   })
   
 }
-
 
 # Create and launch the Shiny app
 shinyApp(ui, server)
